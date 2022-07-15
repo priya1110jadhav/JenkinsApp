26ca3bc008bb0fcf32adf97e96ceb9f13cee6a5b
priya1110jadhav <priyankajadhav1@ternaengg.ac.in>
2022-07-15 11:49:22 +0530

    Add project files.

---
+++ JenkinsApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace JenkinsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hey Priya");
+            Console.ReadKey();
+        }
+    }
+}
